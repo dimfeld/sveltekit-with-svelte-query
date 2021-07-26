6cb6197c2bc5da01f1e40819431f5d5979423a28
Daniel Imfeld <daniel@imfeld.dev>
2021-07-26 18:32:53 +0000

    test case for svelte-query with sveltekit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query';
+
+  const client = new QueryClient();
+</script>
+
+<QueryClientProvider {client}>
+  <slot />
+</QueryClientProvider>
